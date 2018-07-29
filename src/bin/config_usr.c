8f13b87fbf6a620ed0b9b4fc742ee8b111cdd63b
ValliereMagic <ValliereMagic@users.noreply.github.com>
2018-07-29 14:56:22 -0400

    Added get_user_config(void) function to return a parsed version of the user defined configuration file.

--- src/bin/config_usr.c
+++ src/bin/config_usr.c
@@ -1,2 +1,30 @@
 #include "libconfig.h"
+#include <stdio.h>
 
+config_t get_user_config(void) {
+    //initiate a new config
+    config_t user_config;
+    config_init(&user_config);
+
+    FILE* config_file = fopen(CONF_DIR "/WordConscious.conf", "r");
+
+    //make sure that the configuration file exists.
+    if (config_file == NULL) {
+        fprintf(stderr, "Error. Unable to read configuration file from disk.\n");
+        config_clear(&user_config);
+        return user_config;
+    }
+
+    int result = config_read(&user_config, config_file);
+
+    //make sure that the configuration was able to be read successfully.
+    if (result == CONFIG_FALSE) {
+        fprintf(stderr, "Error. Unable to read the file \"WordConscious.conf\" as a "
+                        "configuration file.\n");
+        config_clear(&user_config);
+        return user_config;
+    }
+
+    //return the now parsed configuration
+    return user_config;
+}
---
+++ src/bin/config_usr.h
@@ -0,0 +1,9 @@
+
+#ifndef CONFIG_USR_H
+#define CONFIG_USR_H
+
+//retrieves user configuration from config file.
+//config_t MUST BE DESTROYED USING config_destroy().
+config_t get_user_config(void);
+
+#endif
